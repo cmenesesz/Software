ba590acaba9ffb1f98bc79fa7038a485ed731ae5
cmenesesz <camilomeneses20@gmail.com>
2018-08-23 17:20:05 -0300

    Se agrega conos al control

--- catkin_ws/src/ros_cap/src/controles.py
+++ catkin_ws/src/ros_cap/src/controles.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import serial, time
 import rospy #importar ros para python
 from duckietown_msgs.msg import Twist2DStamped
 from sensor_msgs.msg import Joy
@@ -10,11 +10,23 @@ class Control(object):
                 self.publisher = rospy.Publisher("/duckiebot/wheels_driver_node/car_cmd",Twist2DStamped,queue_size=10)
 		self.subscriber = rospy.Subscriber("/duckiebot/joy/",Joy,self.controlar)
 		self.twist = Twist2DStamped()
+		self.arduino = serial.Serial("/dev/ttyUSB0", 9600)
+		time.sleep(2)
+
 
 	def controlar(self,msg):
 		x=msg.axes[1]           #lineal
-		y=msg.axes[0]*2	#angular
+		y=msg.axes[0]*2	        #angular
 		z=msg.buttons[0]        #freno
+		u=msg.buttons[4]        #cono a la izq
+		v=msg.buttons[5]        #cono a la der
+		p=msg.buttons[1]        #cono parar
+		if u==1:
+			arduino.write('i')
+		if v==1:
+			arduino.write('d')
+		if p==1:
+			arduino.write('p')
 		if z==1:
 			self.twist.v= 0.0
 			self.twist.omega = 0.0
